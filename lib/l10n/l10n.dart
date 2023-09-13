b21b19d129d730ab7c747a76ca9b3c5ed961983e
Edoki Chukwuyem <edokichuks@gmail.com>
2023-09-13 05:49:17 +0100

    arb json files formating

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 class L10n {
   static final all = [
     const Locale('en'),
-    // const Locale('ar'),
-    // const Locale('hi'),
-    // const Locale('es'),
-    // const Locale('de'),
     const Locale('fr'),
   ];
 }
